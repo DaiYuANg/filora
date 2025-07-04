6aff26af5f2c9acfeb9ac611f8ad0c07955ce9e7
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2025-07-04 23:14:13 +0800

    update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    java
-    id("io.quarkus")
+  java
+  id("io.quarkus")
 }
 
 repositories {
-    mavenCentral()
-    mavenLocal()
+  mavenCentral()
+  mavenLocal()
 }
 
 val quarkusPlatformGroupId: String by project
@@ -13,53 +13,53 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-blaze-persistence-bom:${quarkusPlatformVersion}"))
-    implementation("io.quarkus:quarkus-rest")
-    implementation("io.quarkus:quarkus-flyway")
-    implementation("io.quarkus:quarkus-jdbc-mariadb")
-    implementation("io.quarkus:quarkus-scheduler")
-    implementation("io.quarkus:quarkus-smallrye-jwt")
-    implementation("com.blazebit:blaze-persistence-integration-quarkus-3")
-    implementation("io.quarkus:quarkus-jdbc-postgresql")
-    implementation("io.quarkus:quarkus-jdbc-h2")
-    implementation("io.quarkus:quarkus-rest-jackson")
-    implementation("io.quarkus:quarkus-hibernate-orm-panache")
-    implementation("io.quarkus:quarkus-mailer")
-    implementation("io.quarkus:quarkus-agroal")
-    implementation("io.quarkus:quarkus-hibernate-envers")
-    implementation("io.quarkus:quarkus-hibernate-orm")
-    implementation("io.quarkus:quarkus-info")
-    implementation("io.quarkus:quarkus-rest-client")
-    implementation("io.quarkus:quarkus-config-yaml")
-    implementation("io.quarkus:quarkus-jfr")
-    implementation("io.quarkus:quarkus-logging-json")
-    implementation("io.quarkus:quarkus-jdbc-mysql")
-    implementation("io.quarkus:quarkus-smallrye-metrics")
-    implementation("io.quarkus:quarkus-rest-client-jackson")
-    implementation("io.quarkus:quarkus-hibernate-validator")
-    implementation("io.quarkus:quarkus-quartz")
-    implementation("io.quarkus:quarkus-redis-client")
-    implementation("io.quarkus:quarkus-redis-cache")
-    implementation("io.quarkus:quarkus-cache")
-    implementation("io.quarkus:quarkus-smallrye-jwt-build")
-    implementation("io.quarkus:quarkus-arc")
-    testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("io.rest-assured:rest-assured")
+  implementation(enforcedPlatform(libs.quarkus.bom))
+  implementation(enforcedPlatform(libs.quarkus.blaze.persistence.bom))
+  implementation("io.quarkus:quarkus-rest")
+  implementation("io.quarkus:quarkus-flyway")
+  implementation("io.quarkus:quarkus-jdbc-mariadb")
+  implementation("io.quarkus:quarkus-scheduler")
+  implementation("io.quarkus:quarkus-smallrye-jwt")
+  implementation("com.blazebit:blaze-persistence-integration-quarkus-3")
+  implementation("io.quarkus:quarkus-jdbc-postgresql")
+  implementation("io.quarkus:quarkus-jdbc-h2")
+  implementation("io.quarkus:quarkus-rest-jackson")
+  implementation("io.quarkus:quarkus-hibernate-orm-panache")
+  implementation("io.quarkus:quarkus-mailer")
+  implementation("io.quarkus:quarkus-agroal")
+  implementation("io.quarkus:quarkus-hibernate-envers")
+  implementation("io.quarkus:quarkus-hibernate-orm")
+  implementation("io.quarkus:quarkus-info")
+  implementation("io.quarkus:quarkus-rest-client")
+  implementation("io.quarkus:quarkus-config-yaml")
+  implementation("io.quarkus:quarkus-jfr")
+  implementation("io.quarkus:quarkus-logging-json")
+  implementation("io.quarkus:quarkus-jdbc-mysql")
+  implementation("io.quarkus:quarkus-smallrye-metrics")
+  implementation("io.quarkus:quarkus-rest-client-jackson")
+  implementation("io.quarkus:quarkus-hibernate-validator")
+  implementation("io.quarkus:quarkus-quartz")
+  implementation("io.quarkus:quarkus-redis-client")
+  implementation("io.quarkus:quarkus-redis-cache")
+  implementation("io.quarkus:quarkus-cache")
+  implementation("io.quarkus:quarkus-smallrye-jwt-build")
+  implementation("io.quarkus:quarkus-arc")
+  testImplementation("io.quarkus:quarkus-junit5")
+  testImplementation("io.rest-assured:rest-assured")
 }
 
 group = "org.filora"
 version = "1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<Test> {
-    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+  systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
 tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
+  options.encoding = "UTF-8"
+  options.compilerArgs.add("-parameters")
 }
